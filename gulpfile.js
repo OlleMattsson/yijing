352d854d3d7089a95d5e76d00f8ef9726d9ffe53
sleeperAgent <olle.mattsson@codemate.com>
2016-02-01 16:43:36 +0200

    working build setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
 var gulp = require("gulp"),
 	rjs = require("requirejs"),
 	clean = require("gulp-clean"),
-    appDir = "./src",
+    jsoncombine = require("gulp-jsoncombine"),
+    gzip = require("gulp-gzip"),
+    gulpSequence = require('gulp-sequence'),
+    uglify = require('gulp-uglify'),
+    appDir = "src",
     distDir = "./dist"
 	;
 
 gulp.task("optimise", function(callback) {
     rjs.optimize({
-        appDir: appDir,
-        baseUrl: "./",
-        dir: distDir,
-        mainConfigFile: appDir + "/index.js",
+        appDir: "",   // copy everything from here
+        dir: "./js-dist",       // to here
+        baseUrl: "./src",        
+        mainConfigFile: "./src/index.js",
         useStrict: true,
         skipDirOptimize: true,
         removeCombined: true,
-        //optimize: 'none',
+        optimize: 'none',
         modules: [{
             name: "index"
         }],
     }, function(buildResponse) {
-        console.log("build response", buildResponse);
+        console.log("build response: ", buildResponse);
         callback();
     }, callback);
 });
@@ -36,14 +40,62 @@ gulp.task("watch", function() {
     ], ["optimise"]);
 });
 
+gulp.task("copyfiles", function () {
+
+    gulp.src( [ './src/index.html', 
+                './src/style.css'])
+    .pipe( gulp.dest('./dist') )
+
+    gulp.src('./js-dist/index.js')
+    .pipe( uglify() )
+    .pipe( gulp.dest('./dist') )
+    .on('error', function(error) {
+        console.log("uglify", error);
+    })
+
+    gulp.src('./src/static/yijing.json')
+    .pipe( gulp.dest( './dist/static'))
+    
+    gulp.src('./src/bower_components/requirejs/require.js')
+    .pipe( gulp.dest( './dist/bower_components/requirejs'));
+    
+    gulp.src('./src/bower_components/bootstrap/dist/js/bootstrap.min.js')
+    .pipe( gulp.dest('./dist/bower_components/bootstrap/dist/js'));
+    
+    gulp.src('./src/bower_components/bootstrap/dist/css/bootstrap.css')
+    .pipe( gulp.dest( './dist/bower_components/bootstrap/dist/css'));
+        
+})
+
 
 
 gulp.task("clean", function() {
-    gulp.src('./dist', {read: false})
+    gulp.src('./js-dist', {read: false})
     .pipe(clean())
     .on('error', function(error) {
         console.log(error);
     })
 });
 
-gulp.task("default", ["optimise"]);
+gulp.task("cleanjsdist", function() {
+    gulp.src('./js-dist', {read: false})
+    .pipe(clean())
+    .on('error', function(error) {
+        console.log(error);
+    })
+});
+
+gulp.task("yijing", function() {
+    gulp.src("./src/static/iching/*.json")
+        .pipe(jsoncombine("yijing.json",function(data){ 
+            return new Buffer(JSON.stringify(data));
+         }))
+        //.pipe(gzip())
+        .pipe(gulp.dest("./src/static"));
+});
+
+ 
+gulp.task('default', gulpSequence( "optimise", "copyfiles"));
+
+//gulp.task("", ["optimise", "build", "cleanjsdist"]);
+gulp.task("y", ["yijing"]);
